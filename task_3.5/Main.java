3be9be8bba7cce0790fed09d87aebbb2c63269ea
Igor Mayachenkov <igor.mayachenkov@gmail.com>
2021-04-28 23:50:25 +0300

    3.5 рекуррентный обход массива

--- task_3.5/Main.java
+++ task_3.5/Main.java
@@ -1,10 +1,24 @@
 /*
-Задание: Дан массив с числами. Выведите последовательно его элементы используя рекурсию и не используя цикл.
+Задание: Дан массив с числами. 
+Выведите последовательно его элементы используя рекурсию и не используя цикл.
 */
 
 
 public class Main {
     public static void main(String[] args) {
-        int num[] = {4,2,6,2,65}
+        int num[] = {4,2,6,2,65};
+
+        print(num, 0);
+        System.out.println();    
+    }
+
+    public static void print(int[] array, int index){
+        if(index >= array.length) return;
+
+        // Print current
+        System.out.printf("%d ", array[index]);    
+
+        // Reccurent call
+        print(array, index+1);
     }
 }
