a0b052df019de71b452f88ebcb9c9e7f254b96a5
Igor Mayachenkov <igor.mayachenkov@gmail.com>
2021-05-10 23:01:26 +0300

    5.4 ввод из вайла и сортировка списка

--- task_5.4/Main.java
+++ task_5.4/Main.java
@@ -19,8 +19,48 @@
 10
 */
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Collections;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        // напишите тут ваш код
+        // Read filename
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь к файлу");
+        String filename = scanner.nextLine();
+        scanner.close();
+
+        // Read data from file
+        ArrayList<Integer> src = new ArrayList<>();
+        try {
+            File file = new File(filename);
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()) {
+                Integer line = reader.nextInt();
+                src.add(line);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        // Do filter
+        ArrayList<Integer> res = new ArrayList<>();
+        for (Integer item : src) {
+            if(item%2==0)
+                res.add(item);
+        };
+        Collections.sort(res);
+
+        // Print result
+        System.out.println("source : "+src);
+        System.out.println("result : "+res);
+
     }
 }
