69819594c6aa3b0066afbfdac177dad65a8d8b94
Igor Mayachenkov <igor.mayachenkov@gmail.com>
2021-05-10 22:28:42 +0300

    5.3 фильтрация списков

--- task_5.3/Main.java
+++ task_5.3/Main.java
@@ -4,8 +4,23 @@
 */
 
 import java.util.ArrayList;
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
-    
+        // Initialization
+        List<Integer> src = new ArrayList<>(
+            List.of(15, 35, 0, 3, -18, 10, 5) 
+        );
+
+        // Do action
+        List<Integer> res = new ArrayList<>();
+        for (Integer item : src) {
+            if(item>0 && item<10)
+                res.add(item);
+        };
+
+        // Print result
+        System.out.println("source : " + src);
+        System.out.println("result : " + res);
     }
 }
